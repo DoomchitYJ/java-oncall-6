fe2ef1c894aefeb0d096736fe0d29a2562d54aa6
DoomchitYJ <poj1004@inha.edu>
2024-12-13 15:38:09 +0900

    feat(Application): Application 실행 기능 구현

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,7 +1,11 @@
 package oncall;
 
+import oncall.controller.Controller;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        final Controller controller = new Controller();
+        controller.run();
     }
 }
