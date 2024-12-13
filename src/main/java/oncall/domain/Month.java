ed52c9cf14c54089d7d2fe34bd734ada4990ec23
DoomchitYJ <poj1004@inha.edu>
2024-12-13 13:11:45 +0900

    feat(Month): 해당 월의 마지막 날이 며칠인지 확인할 수 있는 변수 추가

--- src/main/java/oncall/domain/Month.java
+++ src/main/java/oncall/domain/Month.java
@@ -6,29 +6,34 @@ import java.util.List;
 
 public enum Month {
 
-    JAN(1, List.of(NEWYEAR)),
-    FEB(2, List.of()),
-    MAR(3, List.of(MOVEMENT)),
-    APR(4, List.of()),
-    MAY(5, List.of(CHILDREN)),
-    JUN(6, List.of(MEMORIAL)),
-    JUL(7, List.of()),
-    AUG(8, List.of(LIBERATION)),
-    SEP(9, List.of()),
-    OCT(10, List.of(FOUNDATION, KOREAN)),
-    NOV(11, List.of()),
-    DEC(12, List.of(CHRISTMAS));
+    JAN(1, 31, List.of(NEWYEAR)),
+    FEB(2, 28, List.of()),
+    MAR(3, 31, List.of(MOVEMENT)),
+    APR(4, 30, List.of()),
+    MAY(5, 31, List.of(CHILDREN)),
+    JUN(6, 30, List.of(MEMORIAL)),
+    JUL(7, 31, List.of()),
+    AUG(8, 31, List.of(LIBERATION)),
+    SEP(9, 30, List.of()),
+    OCT(10, 31, List.of(FOUNDATION, KOREAN)),
+    NOV(11, 30, List.of()),
+    DEC(12, 31, List.of(CHRISTMAS));
 
     private final int month;
+    private final int endDay;
     private final List<Holiday> holidays;
 
-    Month(int month, List<Holiday> holidays) {
+    Month(int month, int endDay, List<Holiday> holidays) {
         this.month = month;
+        this.endDay = endDay;
         this.holidays = holidays;
     }
 
     public int getMonth() {
         return month;
+    }
+    public int getEndDay() {
+        return endDay;
     }
     public List<Holiday> getHolidays() {
         return holidays;
