afd80959049833842da5709e3dbba5800aab59a5
DoomchitYJ <poj1004@inha.edu>
2024-12-13 16:39:32 +0900

    refactor(WorkerPlacer): 메서드 분리를 통한 리팩토링

--- src/main/java/oncall/domain/WorkerPlacer.java
+++ src/main/java/oncall/domain/WorkerPlacer.java
@@ -1,5 +1,8 @@
 package oncall.domain;
 
+import static oncall.domain.Week.SAT;
+import static oncall.domain.Week.SUN;
+
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
@@ -7,47 +10,40 @@ import java.util.List;
 
 public class WorkerPlacer {
 
-    private final Month month;
-    private final Week day;
-    private final Deque<String> weekdayWorkers;
-    private final Deque<String> weekendWorkers;
+    private static final int DAYSOFWEEK = 7;
 
     private static final String MONTH = "월 ";
     private static final String DAY = "일 ";
     private static final String HOLIDAY = "(휴일)";
 
-    public WorkerPlacer(final Month month, final Week day, final List<String> weekdayWorkers, final List<String> weekendWorkers) {
+    private static final List<Integer> WEEKEND_ORDER = List.of(SAT.getOrder(), SUN.getOrder());
+
+    private final Month month;
+    private final Week day;
+    private final Deque<String> weekdayWorkers;
+    private final Deque<String> weekendWorkers;
+
+    public WorkerPlacer(final Month month, final Week day,
+                        final List<String> weekdayWorkers, final List<String> weekendWorkers) {
         this.month = month;
         this.day = day;
         this.weekdayWorkers = new LinkedList<>(weekdayWorkers);
         this.weekendWorkers = new LinkedList<>(weekendWorkers);
     }
 
-    private List<Integer> getHolidays() {
-        List<Holiday> holidays = month.getHolidays();
-        return holidays.stream()
-                .map(Holiday::getDay)
-                .toList();
-    }
-
     public List<String> place() {
+        List<String> result = new ArrayList<>();
+        List<String> resultNames = new ArrayList<>();
+
         String currenMonth = month.getName();
         int currentDay = day.getOrder();
-        List<Integer> currentHoliday = getHolidays();
-
-        List<String> resultNames = new ArrayList<>();
-        List<String> result = new ArrayList<>();
 
         for (int i=1; i<=month.getEndDay(); i++) {
             String todayResult = currenMonth+MONTH;
-
-            String yesterdayWorker = "";
-            if (!resultNames.isEmpty()) {
-                yesterdayWorker = resultNames.getLast();
-            }
+            String yesterdayWorker = getYesterdayWorker(resultNames);
 
             // 평일
-            if (!currentHoliday.contains(i) && currentDay != 5 && currentDay != 6) {
+            if (!isHoliday(i) && !isWeekend(currentDay)) {
                 String todayWorker = weekdayWorkers.pollFirst();
                 weekdayWorkers.addLast(todayWorker);
                 if (todayWorker.equals(yesterdayWorker)) {
@@ -61,7 +57,7 @@ public class WorkerPlacer {
             }
 
             // 공휴일
-            if (currentHoliday.contains(i)) {
+            if (isHoliday(i)) {
                 String todayWorker = weekendWorkers.pollFirst();
                 weekendWorkers.addLast(todayWorker);
                 if (todayWorker.equals(yesterdayWorker)) {
@@ -75,7 +71,7 @@ public class WorkerPlacer {
             }
 
             // 주말
-            if (currentDay == 5 || currentDay == 6) {
+            if (isWeekend(currentDay)) {
                 String todayWorker = weekendWorkers.pollFirst();
                 weekendWorkers.addLast(todayWorker);
                 if (todayWorker.equals(yesterdayWorker)) {
@@ -88,9 +84,36 @@ public class WorkerPlacer {
                 todayResult = todayResult + i + DAY + Week.getNameByOrder(currentDay)+ " " + resultNames.getLast();
             }
 
-            currentDay = (currentDay + 1) % 7;
+            currentDay = updateCurrentDay(currentDay);
             result.add(todayResult);
         }
         return result;
+    }
+
+    private boolean isWeekend(int currentDay) {
+        return WEEKEND_ORDER.contains(currentDay);
+    }
+
+    private boolean isHoliday(int day) {
+        List<Integer> currentHoliday = getHolidays();
+        return currentHoliday.contains(day);
+    }
+
+    private List<Integer> getHolidays() {
+        List<Holiday> holidays = month.getHolidays();
+        return holidays.stream()
+                .map(Holiday::getDay)
+                .toList();
+    }
+
+    private String getYesterdayWorker(List<String> resultNames) {
+        if (!resultNames.isEmpty()) {
+            return resultNames.getLast();
+        }
+        return "";
+    }
+
+    private int updateCurrentDay(int currentDay) {
+        return (currentDay+1) % DAYSOFWEEK;
     }
 }
