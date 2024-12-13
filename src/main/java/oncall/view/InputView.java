cdb1dade22a28b8792080e6456ffc1c1aa7777aa
DoomchitYJ <poj1004@inha.edu>
2024-12-13 12:56:33 +0900

    feat(InputView): 비상 근무자 닉네임을 입력 받는 기능 구현

--- src/main/java/oncall/view/InputView.java
+++ src/main/java/oncall/view/InputView.java
@@ -1,13 +1,27 @@
 package oncall.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputView {
 
     private static final String MONTH_DAY_VIEW = "비상 근무를 배정할 월과 시작 요일을 입력하세요>";
 
+    private static final String WEEKDAY_VIEW = "평일 비상 근무 순번대로 사원 닉네임을 입력하세요>";
+    private static final String WEEKEND_VIEW = "휴일 비상 근무 순번대로 사원 닉네임을 입력하세요>";
+
     public static String readMonthDay() {
         System.out.println(MONTH_DAY_VIEW);
         return Console.readLine().trim();
+    }
+
+    public static List<String> readWorkers() {
+        List<String> input = new ArrayList<>();
+        System.out.println(WEEKDAY_VIEW);
+        input.add(Console.readLine().trim());
+        System.out.println(WEEKEND_VIEW);
+        input.add(Console.readLine().trim());
+        return input;
     }
 }
